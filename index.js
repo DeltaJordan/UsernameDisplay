48eb8f349c1fbf28e835d6d9145ceaedf2ff1e99
Jordan Zeotni <thegreatplanetmojang@gmail.com>
2018-01-31 09:16:31 -0600

    Reworked plugin a bit.
    Moved repeatitve styles to css.
    Removed repeatitive listener.
    Did a few null checks to prevent console error spamming (thanks Curtis c:)

--- index.js
+++ index.js
@@ -4,63 +4,25 @@ const $ = require("jquery");
 class UsernameDisplay extends Plugin {
   constructor(...args){
     super(...args);
-    this.log('Loading listeners');
-    this.messageGroupBind = this.messageGroup.bind(this);
-    window.A.Watcher.on('messageGroup', this.messageGroupBind);
-    window.A.Watcher.on('message', this.messageGroupBind);
+    this.log('Loading listeners...');
     this.processBind = this.process.bind(this);
     window.DI.StateWatcher.on('mutation', this.processBind);
   }
 
   unload() {
-    window.A.Watcher.removeListener('messageGroup', this.messageGroupBind);
-    window.A.Watcher.removeListener('message', this.messageGroupBind);
-  }
+    window.Di.StateWatcher.removeListener('mutation', this.processBind);
 
-  static getReactInstance (node) { return node[ Object.keys(node).find((key) => key.startsWith("__reactInternalInstance")) ] }
-
-  messageGroup(event) {
-
-    var usernameElement = event.element.querySelector(".user-name");
-
-    if (usernameElement.innerHTML === event.message.author.username){
-      return;
-    }
-
-    var parentElement = usernameElement.parentElement;
-
-    if (parentElement.querySelector(".username-tag") != undefined){
-      return;
-    }
-
-    //this.log(element.innerHTML);
-
-    var usernameTagNode = document.createElement("SPAN");
-
-    usernameTagNode.innerHTML = `${event.message.author.username}#${event.message.author.discriminator}`;
-
-    usernameTagNode.classList.add("username-tag");
-
-    usernameTagNode.style.backgroundColor = usernameElement.style.color;
-    usernameTagNode.style.color = "#FFFFFF";
-    usernameTagNode.style.flexShrink = "0";
-    usernameTagNode.style.borderRadius = "3px";
-    usernameTagNode.style.fontSize = "12px";
-    usernameTagNode.style.lineHeight = "16px";
-    usernameTagNode.style.marginLeft = "6px";
-    usernameTagNode.style.padding = "1px 2px";
-    usernameTagNode.style.verticalAlign = "bottom";
-
-    parentElement.appendChild(usernameTagNode);
-
-    //this.log(event.message.author.username);
+    $(".chat .comment").each((i, group) => {
+      var names = $(".username-tag", group).each((i, usernameTagElement) => {
+        usernameTagElement.parentElement.removeChild(usernameTagElement);
+      });
+    });
   }
 
   process(force){
 
     $(".chat .comment").each((i, group) => {
       try{
-        //let author = UsernameDisplay.getReactInstance(group).memoizedProps.children[ 0 ][ 0 ].props.message.author;
 
         var names = $("strong.user-name", group).each((i, usernameElement) => {
 
@@ -70,12 +32,21 @@ class UsernameDisplay extends Plugin {
             return;
           }
 
+          // Small hack until XClass works on channel switch ;P
           var regexUserId = /https:\/\/cdn.discordapp.com\/avatars\/([0-9]+)\//i;
 
           var avatarElement = group.parentElement.querySelector(".avatar-large");
-          var match = avatarElement.style.backgroundImage.match(regexUserId)[1];
+          var match = avatarElement.style.backgroundImage.match(regexUserId);
 
-          let user = window.DI.Helpers.resolveMention(`<@${match}>`);
+          if (!match){
+            return;
+          }
+
+          //this.log(match);
+
+          var userIdString = match[1];
+
+          let user = window.DI.Helpers.resolveMention(`<@${userIdString}>`);
 
           if (usernameElement.innerHTML === user.username){
             return;
@@ -90,14 +61,6 @@ class UsernameDisplay extends Plugin {
           usernameTagNode.classList.add("username-tag");
 
           usernameTagNode.style.backgroundColor = usernameElement.style.color;
-          usernameTagNode.style.color = "#FFFFFF";
-          usernameTagNode.style.flexShrink = "0";
-          usernameTagNode.style.borderRadius = "3px";
-          usernameTagNode.style.fontSize = "12px";
-          usernameTagNode.style.lineHeight = "16px";
-          usernameTagNode.style.marginLeft = "6px";
-          usernameTagNode.style.padding = "1px 2px";
-          usernameTagNode.style.verticalAlign = "bottom";
 
           parentElement.appendChild(usernameTagNode);
         });
