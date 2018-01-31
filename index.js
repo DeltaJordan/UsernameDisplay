88d312344d23c24679811b35a85a01c7fe835d93
Jordan Zeotni <thegreatplanetmojang@gmail.com>
2018-01-31 10:58:51 -0600

    Added color check to make bright colors readable. Black for brighter backgrounds, white for darker.

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ class UsernameDisplay extends Plugin {
     $(".chat .comment").each((i, group) => {
       try{
 
+        //this.log((0.299*R + 0.587*G + 0.114*B));
+
         var names = $("strong.user-name", group).each((i, usernameElement) => {
 
           var parentElement = usernameElement.parentElement;
@@ -61,6 +63,32 @@ class UsernameDisplay extends Plugin {
           usernameTagNode.classList.add("username-tag");
 
           usernameTagNode.style.backgroundColor = usernameElement.style.color;
+
+          var regex = /[0-9]+/gi;
+
+          var matchColors = usernameElement.style.color.match(regex);
+
+          var textColor = "rgb(255, 255, 255)";
+
+          this.log(`backgroundColor: ${usernameElement.style.color}`)
+          if (matchColors[0]){
+            this.log(`Match 1: ${matchColors[0]}`);
+            var R = parseInt(matchColors[0]);
+            if (matchColors[1]){
+              this.log(`Match 2: ${matchColors[1]}`);
+              var G = parseInt(matchColors[1]);
+              if (matchColors[2]){
+                this.log(`Match 3: ${matchColors[2]}`);
+                var B = parseInt(matchColors[2]);
+                if ((0.299*R + 0.587*G + 0.114*B) > 176){
+                  this.log((0.299*R + 0.587*G + 0.114*B));
+                  textColor = "rgb(0, 0, 0)";
+                }
+              }
+            }
+          }
+
+          usernameTagNode.style.color = textColor;
 
           parentElement.appendChild(usernameTagNode);
         });
